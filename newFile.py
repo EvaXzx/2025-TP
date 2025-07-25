a36c26278bf94f45ebe53b724ff2d915bcc5b6b2
EvaXzx <xu.zixuan.hh@gmail.com>
2025-07-24 22:24:00 -0400

    Create newFile.py

---
+++ newFile.py
@@ -0,0 +1,5 @@
+print('hello I am Eva')
+#I spent most of my time setting up VS code and python, 
+# so not a lot of coding yet, I've started watching the default project
+#video and will start coding tomorrow
+
