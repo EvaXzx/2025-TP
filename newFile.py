4232da39865547e7d33ccff658664376881aff0e
EvaXzx <xu.zixuan.hh@gmail.com>
2025-07-24 23:11:34 -0400

    Update newFile.py

--- newFile.py
+++ newFile.py
@@ -1,5 +1,14 @@
+from cmu_graphics import * 
 print('hello I am Eva')
 #I spent most of my time setting up VS code and python, 
 # so not a lot of coding yet, I've started watching the default project
 #video and will start coding tomorrow
 
+def redrawAll(app):
+    drawRect(50, 50, 100, 200, fill = 'blue')
+
+
+def main():
+    runApp()
+
+main()
