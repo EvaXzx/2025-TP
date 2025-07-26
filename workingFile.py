b46ff5908bc9055cdce51b1fe2b2a95fbe7fd5a7
EvaXzx <xu.zixuan.hh@gmail.com>
2025-07-26 12:12:39 -0400

    Create workingFile.py

---
+++ workingFile.py
@@ -0,0 +1,51 @@
+#Draw Board
+from cmu_graphics import *
+
+def onAppStart(app):
+    app.rows = 5
+    app.cols = 5
+    app.boardLeft = 50
+    app.boardTop = 75
+    app.boardWidth = 400
+    app.boardHeight = 400
+    app.cellBorderWidth = 2
+    app.width = 500
+    app.height = 500
+
+def redrawAll(app):
+    drawBoard(app)
+    drawBoardBorder(app)
+
+def drawBoard(app):
+    for row in range(app.rows):
+        for col in range(app.cols):
+            drawCell(app, row, col)
+
+def drawBoardBorder(app):
+  # draw the board outline (with double-thickness):
+  drawRect(app.boardLeft, app.boardTop, app.boardWidth, app.boardHeight,
+           fill=None, border='black',
+           borderWidth=2*app.cellBorderWidth)
+
+def drawCell(app, row, col):
+    cellLeft, cellTop = getCellLeftTop(app, row, col)
+    cellWidth, cellHeight = getCellSize(app)
+    drawRect(cellLeft, cellTop, cellWidth, cellHeight,
+             fill=None, border='black',
+             borderWidth=app.cellBorderWidth)
+
+def getCellLeftTop(app, row, col):
+    cellWidth, cellHeight = getCellSize(app)
+    cellLeft = app.boardLeft + col * cellWidth
+    cellTop = app.boardTop + row * cellHeight
+    return (cellLeft, cellTop)
+
+def getCellSize(app):
+    cellWidth = app.boardWidth / app.cols
+    cellHeight = app.boardHeight / app.rows
+    return (cellWidth, cellHeight)
+
+def main():
+    runApp()
+
+main()
